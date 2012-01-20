4f413451af36b5b1a18330a98e3358698be3a858
Daron Dima <daron.dima@gmail.com>
2012-01-21 01:56:44 +0200

    functions declorations added

--- SimpleFileSystem.h
+++ SimpleFileSystem.h
@@ -2,6 +2,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+
+BOOL initFS(char* OsFileName, int SizeInBlocks);
+BOOL attachfs(char* OsFileName);
+BOOL detachfs();
+
+void* create (char* FileName, int SizeBlocks);
+void* open (char* FileName, char RWflag);
+int read (int fd, int BlockNum, char* Buff, int BuffLengh);
+int write (int fd, int BlockNum, char* Buff, int BuffLengh);
+BOOL close(int fd);
+
+BOOL rename(char* OldFileName, char* NewFileName);
+BOOL deleteFile(char* FileName);
+
+
 struct file {
 	char* name;
 	int size;
@@ -9,7 +25,6 @@ struct file {
 
 struct directory {
 	char* name;
-	file* filearray;
-
+	file* fileArray;
 }
 
